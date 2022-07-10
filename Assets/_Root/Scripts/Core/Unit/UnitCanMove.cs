cdda93e7c1f9a02a7713d807b9793590bd4deaaa
Nick2712 <53843441+Nick2712@users.noreply.github.com>
2022-07-10 09:30:07 +0300

    сделал дз4

--- Assets/_Root/Scripts/Core/Unit/UnitCanMove.cs
+++ Assets/_Root/Scripts/Core/Unit/UnitCanMove.cs
@@ -1,4 +1,6 @@
 using NikolayTrofimov_StrategyGame.Abstractions;
+using NikolayTrofimov_StrategyGame.Utils;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,15 +11,30 @@ namespace NikolayTrofimov_StrategyGame.Core
     {
         [SerializeField] private UnitMovementStop _stop;
         [SerializeField] private Animator _animator;
+        [SerializeField] private UnitCanStop _stopCommandExecutor;
 
         private static readonly int _walk = Animator.StringToHash("Walk");
         private static readonly int _idle = Animator.StringToHash("Idle");
+
 
         public override async void ExecuteSpecificCommand(IMoveCommand command)
         {
             GetComponent<NavMeshAgent>().destination = command.Target;
             _animator.SetTrigger(_walk);
-            await _stop;
+
+            _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await _stop.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
+            }
+            catch
+            {
+                GetComponent<NavMeshAgent>().isStopped = true;
+                GetComponent<NavMeshAgent>().ResetPath();
+            }
+            _stopCommandExecutor.CancellationTokenSource = null;
+
             _animator.SetTrigger(_idle);
         }
     }
