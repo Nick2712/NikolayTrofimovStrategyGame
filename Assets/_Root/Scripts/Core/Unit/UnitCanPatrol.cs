d652e592ac162da878d39e1d7a499590035eb1ee
Nick2712 <53843441+Nick2712@users.noreply.github.com>
2022-07-19 08:14:33 +0300

    сделал дз1

--- Assets/_Root/Scripts/Core/Unit/UnitCanPatrol.cs
+++ Assets/_Root/Scripts/Core/Unit/UnitCanPatrol.cs
@@ -1,15 +1,49 @@
 using NikolayTrofimov_StrategyGame.Abstractions;
+using NikolayTrofimov_StrategyGame.Utils;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace NikolayTrofimov_StrategyGame.Core
 {
     public sealed class UnitCanPatrol : CommandExecutorBase<IPatrolCommand>
     {
+        [SerializeField] private UnitMovementStop _stop;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private UnitCanStop _stopCommandExecutor;
+
+
         public override async Task ExecuteSpecificCommand(IPatrolCommand command)
         {
-            Debug.Log("патрулирую");
+            var point1 = command.From;
+            var point2= command.To;
+
+            while(true)
+            {
+                GetComponent<NavMeshAgent>().destination = point2;
+                _animator.SetTrigger(Animator.StringToHash("Walk"));
+                _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+
+                try
+                {
+                    await _stop.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
+                }
+                catch
+                {
+                    GetComponent<NavMeshAgent>().isStopped = true;
+                    GetComponent<NavMeshAgent>().ResetPath();
+                    break;
+                }
+
+                var temp = point1;
+                point1 = point2;
+                point2 = temp;
+            }
+
+            _stopCommandExecutor.CancellationTokenSource = null;
+            _animator.SetTrigger(Animator.StringToHash("Idle"));
         }
     }
 }
